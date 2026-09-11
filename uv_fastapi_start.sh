4497047a0a2235e0b01358e5b338035b81185f82
euiyounghwang <marieuig@gmail.com>
2026-09-11 17:53:27 -0500

    Update

--- uv_fastapi_start.sh
+++ uv_fastapi_start.sh
@@ -2,5 +2,21 @@
 
 set -eu
 
+
+# Activate virtualenv && run serivce
+SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+VENV=".venv"
+# VENV=".test_venv"
+
+# Python 3.11.7 with Window
+if [ -d "$VENV/bin" ]; then
+    source $VENV/bin/activate
+else
+    source $VENV/Scripts/activate
+fi
+
+export PYTHONDONTWRITEBYTECODE=1
+
 # uvicorn 실행 시 --reload 옵션을 주어야 소스 변경을 감지합니다.
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload

09b1f28030e6d17908ab87fe592f909298b46a50
euiyounghwang <marieuig@gmail.com>
2026-09-11 22:50:27 -0500

    Update

--- uv_fastapi_start.sh
+++ uv_fastapi_start.sh
@@ -19,4 +19,8 @@ fi
 export PYTHONDONTWRITEBYTECODE=1
 
 # uvicorn 실행 시 --reload 옵션을 주어야 소스 변경을 감지합니다.
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+# uvicorn app.main:app --host 0.0.0.0 --port 8001 --reload
+uv run uvicorn app.main:app --reload --host=0.0.0.0 --port=8001 --workers 1
+
+# gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8001 --workers 1
+# gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8001 --workers 4
